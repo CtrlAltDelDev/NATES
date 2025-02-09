89ccff4f5c81335b0dbccf39690d3485148b69b5
Nate <sparksnathan84@gmail.com>
2025-02-09 09:40:01 -0800

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,11 @@
+        <nav class="content-area">
+            <img src="images/logo.png" alt="hero image of speaker at summit">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="speaker.php">Speakers</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="hero-container">
+            <img src="images/MediaCoverage-1410x790.jpg" alt="Image of NATES Summit">
+        </div>
---
+++ speaker.php
@@ -0,0 +1,36 @@
+<?php include 'header.php'; ?>
+<?php include 'nav.php'; ?>
+<div class="content-area">
+    <div class="speaker-container">
+        <div class="speaker" id="photo"><img src="images/Speaker1.png"></div>
+        <div class="speaker" id="bio">
+            <h1>Biography</h1>
+            <p>
+                Jim – Engineer at Tesla Motor Company
+
+                With over 26 years of experience in the automotive industry, Jim is a recognized expert in EV repair. As an Engineer
+                and Industry Consultant, he has worked extensively in the area of EV battery technology.
+
+                Jim is passionate about technical education and workforce development, having trained upward of 1000 technicians and
+                industry professionals through his work with Tesla Motors training program. He is
+                dedicated to bridging the gap between evolving automotive technology and hands-on application, ensuring that technicians
+                stay ahead of industry trends.
+
+                When not working in the shop or classroom, Jim enjoys restoring classic cars and upgrading their powertrain to all electric.
+
+                Join Jim for an engaging session that will equip you with practical knowledge and forward-thinking strategies
+                to excel in the EV sector of today’s automotive industry.
+            </p>
+        </div>
+    </div>
+    <div class="speaker" id="details">
+        <p>
+            Presentation Topics: Advancing Automotive Technical Education and Industry Innovation
+            At the NATES Summit, Jim will deliver an in-depth exploration of key trends shaping the automotive repair and service industry.
+            This session will provide attendees with the knowledge, skills, and forward-thinking strategies necessary to excel in a rapidly evolving field.
+            With a focus on both emerging technologies and the foundational principles that every technician must master,
+            this presentation will cover the following critical topics related new advances in EV battery technologies.
+        </p>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
