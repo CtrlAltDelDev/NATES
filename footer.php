4c4a17768d911538b35371477b7ef9b630fa8328
nate <sparksnathan84@gmail.com>
2025-02-09 20:06:11 -0800

    clean up

--- footer.php
+++ footer.php
@@ -1,12 +1,11 @@
-<footer>
-    <div class ="content-area">
-    </div>
-</footer>
-<div id="copy">
-    <div class="content-area">
-                    <p>&copy; 2025 NATES. All rights reserved.</p>
-
+    <footer>
+        <div class ="content-area">
         </div>
-</div>
+    </footer>
+    <div id="copy">
+        <div class="content-area">
+            <p>&copy; 2025 NATES. All rights reserved.</p>
+        </div>
+    </div>
 </body>
 </html>
